1c2311cfcfc6870d90d476fa22773bf5a33acc9b
donguyen6c <donguyen6c@gmail.com>
2024-08-13 20:28:17 +0700

    HOÀN THÀNH 95% MAINPAGE

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-const soundToggle = document.getElementById("soundToggle");
-const backgroundAudio = document.getElementById("backgroundAudio");
-const toggleIcon = document.getElementById("toggleIcon");
-let isPlaying = false;
+const sound = document.getElementById("sound");
+const audio = document.getElementById("audio");
+const icon = document.getElementById("icon");
+let playing = false;
 
 // Khi nhấn vào nút
-soundToggle.addEventListener("click", function () {
-  if (isPlaying) {
-    backgroundAudio.pause(); // Dừng âm thanh
-    toggleIcon.src = "img/valorant_icon/off_sound.png"; // Đổi hình ảnh thành "Play"
-    toggleIcon.alt = "Play"; // Đổi mô tả hình ảnh thành "Play"
+sound.addEventListener("click", function () {
+  if (playing) {
+    audio.pause(); // Dừng âm thanh
+    icon.src = "img/valorant_icon/off_sound.png"; // Đổi hình ảnh thành "Play"
+    icon.alt = "Play"; // Đổi mô tả hình ảnh thành "Play"
   } else {
-    backgroundAudio.currentTime = 0; // Đặt âm thanh về đầu
-    backgroundAudio.play(); // Phát âm thanh
-    toggleIcon.src = "img/valorant_icon/sound.png"; // Đổi hình ảnh thành "Pause"
-    toggleIcon.alt = "Pause"; // Đổi mô tả hình ảnh thành "Pause"
+    audio.currentTime = 0; // Đặt âm thanh về đầu
+    audio.play(); // Phát âm thanh
+    icon.src = "img/valorant_icon/sound.png"; // Đổi hình ảnh thành "Pause"
+    icon.alt = "Pause"; // Đổi mô tả hình ảnh thành "Pause"
   }
-  isPlaying = !isPlaying; // Đảo ngược trạng thái
+  playing = !playing; // Đảo ngược trạng thái
 });
+
+
+
+
+
+
