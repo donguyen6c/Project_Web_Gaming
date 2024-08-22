f8494415b3007473a50c5d799d761abf5c21cb6a
donguyen6c <donguyen6c@gmail.com>
2024-08-22 17:58:43 +0700

    hoàn thành 4 trang web, tối ưu, điều chỉnh lặt vặt, chưa reponsive
    main page, guide, agents, media

--- support/support.js
+++ support/support.js
@@ -7,19 +7,20 @@ let playing = false;
 sound.addEventListener("click", function () {
   if (playing) {
     audio.pause(); // Dừng âm thanh
-    icon.src = "img/valorant_icon/off_sound.png"; // Đổi hình ảnh thành "Play"
+    icon.src = "../img/valorant_icon/off_sound.png"; // Đổi hình ảnh thành "Play"
     icon.alt = "Play"; // Đổi mô tả hình ảnh thành "Play"
   } else {
     audio.currentTime = 0; // Đặt âm thanh về đầu
     audio.play(); // Phát âm thanh
-    icon.src = "img/valorant_icon/sound.png"; // Đổi hình ảnh thành "Pause"
+    icon.src = "../img/valorant_icon/sound.png"; // Đổi hình ảnh thành "Pause"
     icon.alt = "Pause"; // Đổi mô tả hình ảnh thành "Pause"
   }
   playing = !playing; // Đảo ngược trạng thái
 });
 
-
-
-
-
-
+const choingay = document.getElementById("footer_button");
+// Gắn sự kiện 'click' cho thẻ <a>
+choingay.addEventListener("click", function (event) {
+  // Hiển thị thông báo khi nhấn vào thẻ <a>
+  alert("GAME SẮP RA MẮT VÀO 30/02/2025!!!!");
+});
